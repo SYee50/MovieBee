bcbcc44062905d6826b4d907012ced9f8f1947bf
SYee50 <sandra.yee001@gmail.com>
2022-09-23 06:17:15 -0400

    Added collection view and navigation bar

---
+++ MovieBee/MovieDetailsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MovieDetailsViewController.swift
+//  MovieBee
+//
+//  Created by Sandra Yee on 9/23/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        // get url
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        
+        // setup posterView
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        posterView.af.setImage(withURL: posterURL!)
+        
+        // setup backdropView
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af.setImage(withURL: backdropURL!)
+        
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
